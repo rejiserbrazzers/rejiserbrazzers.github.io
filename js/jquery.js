8ba1d576a296e4c76ad7925db57a843373105b66
rejiserbrazzers <40791904+rejiserbrazzers@users.noreply.github.com>
2018-11-14 14:59:21 +0300

    Add files via upload

---
+++ js/jquery.js
@@ -0,0 +1,9 @@
+﻿$(window).on('resize', function(){
+      var win = $(this); //this = window
+      if (win.height() >= 468) { 
+    
+     document.getElementById("mob_show").style.display = "none";
+
+      }
+     
+});
--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
-﻿
-        function viewDiv(){
+﻿   
+function viewDiv(){
   document.getElementById("mob_show").style.display = "block";
 };
         function hideDiv(){
   document.getElementById("mob_show").style.display = "none";
 };
+
+
+window.onresize = function() {
+    if (window.innerHeight >= 820) { /* ... */ }
+    if (window.innerWidth <= 1280) {  /* ... */ }
+}
